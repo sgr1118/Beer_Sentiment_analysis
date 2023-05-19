18be0c36b774bd081440b7075fb9183e400be8d6
GiRak Son <104058335+sgr1118@users.noreply.github.com>
2023-05-20 02:34:05 +0900

    Add files via upload

---
+++ Sentiment_analsis_result/metrics_weighted.py
@@ -0,0 +1,23 @@
+# macro 적용 - gpt api and MultinomialNB_labeling
+
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.metrics import classification_report
+import pandas as pd
+
+df_result = pd.read_csv('/home/beerlab/result_run_len_.csv')
+y_true = df_result['original']
+y_pred = df_result['predicted']
+
+f1 = f1_score(y_true, y_pred, average='weighted')
+acc = accuracy_score(y_true, y_pred)
+precision = precision_score(y_true, y_pred, average='weighted')
+recall = recall_score(y_true, y_pred, average='weighted')
+
+report = classification_report(y_true, y_pred)
+
+print('result_run_len_ classification_report의 결과')
+print(f"f1_score: {f1:.3f}, accuracy: {acc:.3f}, precision: {precision:.3f}, recall: {recall:.3f}")
+print(len(df_result))
+print(df_result['original'].value_counts())
+print(df_result['predicted'].value_counts())
+print(report)
